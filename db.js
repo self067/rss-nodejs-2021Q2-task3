23bb4dea4bffbdf0deae496fa43e9fe9ed39b681
self067 <seltor@mail.ru>
2021-05-19 16:13:22 +0300

    feat: add dotenv, .env & fix db.js

--- db.js
+++ db.js
@@ -1,16 +1,22 @@
-const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
-    dialect: 'postgres'
-})
+const Sequelize = require("sequelize");
+require("dotenv").config();
+
+const sequelize = new Sequelize(
+  process.env.DB,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "postgres",
+  }
+);
 
 sequelize.authenticate().then(
-    function success() {
-        console.log("Connected to DB");
-    },
+  function success() {
+    console.log("Connected to DB");
+  },
 
-    function fail(err) {
-        console.log(`Error: ${err}`);
-    }
-)
+  function fail(err) {
+    console.log(`Error: ${err}`);
+  }
+);
